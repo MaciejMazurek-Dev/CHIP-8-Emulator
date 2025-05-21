eb3c4cb452f5b79a438ed90a5a8e8b5863c4b8ba
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-05-21 17:15:25 +0200

    Fix timer overflow bug

--- src/Chip8_Library/Timers.cs
+++ src/Chip8_Library/Timers.cs
@@ -5,10 +5,8 @@ namespace Chip8_Library
 {
     internal class Timers
     {
-        
         public byte delayTime = 0;
         public byte soundTime = 0;
-
 
         private Timer _delayTimer = new Timer(1000);
         private Timer _soundTimer = new Timer(1000);
@@ -19,32 +17,35 @@ namespace Chip8_Library
             _delayTimer.Elapsed += OnDelayTimedEvent;
             _delayTimer.Enabled = true;
         }
-
         public void SetSoundTimer(byte second)
         {
             soundTime = second;
             _soundTimer.Elapsed += OnSoundTimedEvent;
             _soundTimer.Enabled = true;
         }
-
         private void OnDelayTimedEvent(object source, ElapsedEventArgs e)
         {
-            delayTime--;
-            if(delayTime < 1)
+            if(delayTime > 1)
+            {
+                delayTime--;
+            }
+            else
             {
                 _delayTimer.Enabled = false;
+                delayTime = 0;
             }
         }
-
         private void OnSoundTimedEvent(object source, ElapsedEventArgs e)
         {
-            soundTime--;
-            if(soundTime < 1)
+            if(soundTime > 1)
+            {
+                soundTime--;
+            }
+            else
             {
                 _soundTimer.Enabled = false;
+                soundTime = 0;
             }
         }
-
-
     }
 }
